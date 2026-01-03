7c23e25b894f2e1293702c7265297dd21bdb19f5
alaaSnayel <alaanayel111@gmail.com>
2026-01-03 09:47:09 +0200

    implement PatientResponseDTO

---
+++ patient-service/src/main/java/com/example/patient_service/service/PatientService.java
@@ -0,0 +1,25 @@
+package com.example.patient_service.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.patient_service.dto.PatientResponseDTO;
+import com.example.patient_service.model.Patient;
+import com.example.patient_service.repository.PatientRepository;
+
+@Service
+public class PatientService {
+  private PatientRepository patientRepository;
+
+  public PatientService(PatientRepository patientRepository) {
+    this.patientRepository = patientRepository;
+  }
+  
+  public List<PatientResponseDTO> getAllPatients() {
+    List<Patient> patients = patientRepository.findAll();
+
+    return null;
+    
+  }
+}
