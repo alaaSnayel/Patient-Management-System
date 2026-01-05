05ae73e5a4de0a023b1afb146a08dc6608b6b6cf
alaaSnayel <alaanayel111@gmail.com>
2026-01-05 09:36:59 +0200

    fix bug at patient controller-post patient

--- patient-service/src/main/java/com/example/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/example/patient_service/controller/PatientController.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.patient_service.dto.PatientRequestDTO;
 import com.example.patient_service.dto.PatientResponseDTO;
 import com.example.patient_service.service.PatientService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/patients")
@@ -24,4 +31,12 @@ public class PatientController {
     List<PatientResponseDTO> patients = patientService.getPatients();
     return ResponseEntity.ok().body(patients);
   }
+
+  @PostMapping
+  public ResponseEntity<PatientResponseDTO> postPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+    PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+    
+    return ResponseEntity.ok().body(patientResponseDTO);
+  }
+  
 }
