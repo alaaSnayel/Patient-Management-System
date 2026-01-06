4aadfca2d0445e48d5b8fd83a868e16e063fb82a
alaaSnayel <alaanayel111@gmail.com>
2026-01-06 15:23:15 +0200

    delete patient

--- patient-service/src/main/java/com/example/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/example/patient_service/controller/PatientController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,4 +56,9 @@ public class PatientController {
     return ResponseEntity.ok().body(patientResponseDTO);
   }
   
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+    patientService.deletePatient(id);
+    return ResponseEntity.noContent().build();
+  }
 }
